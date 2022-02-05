21c0928197c0f1a3b0dcaf034032816268aa7c38
artemilchenko <artemilchenko999@gmail.com>
2022-02-05 02:12:20 +0200

    gdhfeh8uue

--- pages/index.js
+++ pages/index.js
@@ -525,8 +525,8 @@ export default function Home() {
                             </div>
                             <div className={'flex flex-col justify-center items-center'}>
                                 <div className={'relative flex justify-end items-end w-48 h-48 md:w-96 md:h-96'}>
-                                    <div
-                                        className={`absolute z-10 w-48 h-48 md:w-96 md:h-96 flex justify-end rotate-${setArrow()} transform scale-90 transition-transform duration-1000`}>
+                                    <div style={{transform: `rotate(${setArrow()}deg)`}}
+                                        className={`absolute z-10 w-48 h-48 md:w-96 md:h-96 flex justify-end transform scale-90 transition-transform duration-1000`}>
                                         <div className={'w-12 h-12 bg-red-900'}></div>
                                     </div>
                                     <div
